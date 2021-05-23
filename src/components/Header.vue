6b4e1adde150467032a49adb774debdb36eec86f
Shivelle <shivelle@gmx.net>
2021-05-23 15:49:46 +0200

    init project, setup basic header, add loading screen

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="bg-indigo-500 text-center text-white p-5 mb-12 tracking-wide">
+    <div class="text-3xl md:text-4xl font-semibold">
+      📈 Current Covid Stats
+    </div>
+
+    <div class="mt-2">
+      Stay up to date with the current covid stats through the 
+      <a href="https://covid19api.com" class="text-indigo-900 font-bold not-italic" target="_blank">Covid19 API</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    Show data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <img :src="require('../assets/spinner.gif')" class="w-22 m-auto" alt="spinner gif">
+    <div class="mt-6">
+      <span class="bg-yellow-300 px-4 py-1 border-0 rounded-lg">
+        Fetching data ...
+      </span>
+    </div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {},
+    data () {
+      return {
+        loading: true,
+        title: 'Global',
+        dataDate: '',
+        stats: {},
+        countries: []
+      };
+    },
+    methods: {
+      async fetchCovidData() {
+        const res = await fetch('https://api.covid19api.com/summary');
+        const data = await res.json();
+        return data
+      },
+    },
+    async created() {
+      const data = await this.fetchCovidData();
+      this.dataDate = data.dataDate
+      this.stats = data.Global
+      this.countries = data.countries
+      this.loading = true
+    }
   }
-}
 </script>
